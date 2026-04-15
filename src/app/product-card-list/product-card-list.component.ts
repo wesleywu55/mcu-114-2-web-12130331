d4d20c90322d07f8a6e498438debb2de42bf3b37
wesleywu55 <wesleywu5555@gmail.com>
2026-04-15 17:34:10 +0800

    feat(pagination): 新增分頁元件

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 
 import { Product } from '../model/product';
+import { PaginationComponent } from '../pagination/pagination.component';
 import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-product-card-list',
-  imports: [ProductCardComponent],
+  imports: [PaginationComponent, ProductCardComponent],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  @Input({ required: true })
+  @Input()
   products!: Product[];
+
+  pageIndex = 1;
 }
