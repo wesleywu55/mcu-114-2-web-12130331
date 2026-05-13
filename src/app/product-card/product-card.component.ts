51673a08defe567f2245ddcfab7ca421f081b617
wesleywu55 <wesleywu5555@gmail.com>
2026-05-13 17:30:49 +0800

    feat(product): 變更產品編號型別為字串

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -9,7 +9,7 @@ import { Component, HostBinding, booleanAttribute, input, numberAttribute, outpu
   host: { class: 'app-product-card' },
 })
 export class ProductCardComponent {
-  readonly id = input.required<number, string | number>({ transform: numberAttribute });
+  readonly id = input.required<string>();
 
   readonly productName = input<string>();
 
